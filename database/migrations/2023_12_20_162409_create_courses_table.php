91ec8eb22ef2c481b4bcae5bc49b24039b9292f6
Audrey Axel Ristyanto <audrey.axel@suitmedia.com>
2023-12-20 23:33:57 +0700

    Added migration table

---
+++ database/migrations/2023_12_20_162409_create_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course', function (Blueprint $table) {
+            $table->string('code')->primary();
+            $table->string('name');
+            $table->integer('course_credit');
+            $table->integer('lab_credit');
+            $table->string('type');
+            $table->text('short_description');
+            $table->text('minimal_requirement');
+            $table->text('study_material_summary');
+            $table->text('learning_media');
+            $table->string('study_program');
+            $table->integer('creator_user_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
