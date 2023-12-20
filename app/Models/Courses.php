057c353ae86b5898b49f9a906e5482f4d707268b
Audrey Axel Ristyanto <audrey.axel@suitmedia.com>
2023-12-21 03:00:59 +0700

    changed

--- app/Models/Courses.php
+++ app/Models/Courses.php
@@ -17,26 +17,11 @@ class Courses extends Model
 
     protected $fillable = [
         'code', 'name', 'course_credit', 'lab_credit', 'type', 'short_description',
-        'minimal_requirement', 'study_material_summary', 'learning_media', 'study_program', 'creator_user_id'
+        'minimal_requirement', 'study_material_summary', 'learning_media', 'study_program',
+        'creator_user_id'
     ];
 
-
-    // public function studyProgram()
-    // {
-    //     return $this->belongsTo(StudyProgram::class, 'study_program_id');
-    // }
-
-    // public function creator()
-    // {
-    //     return $this->belongsTo(User::class, 'creator_user_id');
-    // }
-
-    // public function courseClasses(){
-    //     return $this->hasMany(CourseClass::class);
-    // }
-
-    // public function syllabi()
-    // {
-    //     return $this->hasMany(Syllabus::class);
-    // }
+    protected $casts = [
+        'code' => 'string',
+    ];
 }
